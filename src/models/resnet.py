e215b87eba36e3108d6245266d02f096d1a3b6fb
Julian <jwsprietsma@gmail.com>
2026-05-15 14:33:15 +0200

    Added resnet file

---
+++ src/models/resnet.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+from base import BaseModel
+from torchvision.models import resnet18
+from src.data.dataset import ChestXRayDataset
+
+class resnet(BaseModel, nn.Module):
+    def __init__(self, dataset: ChestXRayDataset, learning_rate: float = 0.001):
+        BaseModel.__init__(self, dataset)
+        nn.Module.__init__(self)
+
+        self.model = resnet18()
