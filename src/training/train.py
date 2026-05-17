ea979bd2055bb744d3cb323ae015e46c3b5dd91e
Anneke Naseef <a.c.naseef@student.rug.nl>
2026-05-17 17:46:02 +0200

    Test commit with partial train.py and trainer.py files to check branch functioning

--- src/training/train.py
+++ src/training/train.py
@@ -1,1 +1,14 @@
 # TODO: An all-purpose training loop for models.
+from src.constants import LOGGER
+from models.CNN import CNN
+
+dataset = ...
+model = CNN(dataset) #placeholder, should be generic in the end
+epoch_number = 0
+
+EPOCHS = 5
+
+best_vloss = 1_000_000.
+
+for epoch in range(EPOCHS):
+    LOGGER.info(f"EPOCH {epoch_number + 1}:")
